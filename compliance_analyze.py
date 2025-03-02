ba02bdd0eadebbae0a20aa1e80ddd106f4494f2c
wadeling <wadeling@gmail.com>
2025-03-02 17:40:34 +0800

    add aws benchmark v3

--- compliance_analyze.py
+++ compliance_analyze.py
@@ -4,9 +4,13 @@ import time
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+# specify cloud platform
+specify_cloud_platform = 'aws'
+
+
 # framework
 framework = 'cis'
-new_csv_field_name = f'{framework}Standard'
+new_csv_field_name = f'{framework}{specify_cloud_platform}Standard'
 
 
 # 定义API的URL和认证令牌
@@ -14,10 +18,11 @@ api_url = 'https://cloud.fastgpt.cn/api/v1/chat/completions'
 auth_token_file = f'{framework}_app_key'
 
 # 定义CSV文件路径
-test_csv_file_path = 'plugin_with_cis_failed_2.csv'
-out_csv_file_path = f'plugin_with_{framework}_deepseek_reasoner.csv'
+test_csv_file_path = 'plugin_aws_failed.csv'
+out_csv_file_path = f'plugin_with_{framework}_{specify_cloud_platform}_deepseek_reasoner.csv'
 
 concurrent_num = 20
+max_retries = 3  # 最大重试次数
 
 def read_token_file(file_path):
     """从指定的文件中读取内容并返回。"""
@@ -49,12 +54,18 @@ def send_chat_request(api_url, auth_token, chat_id, user_message, stream=False, 
         ]
     }
 
-    try:
-        response = requests.post(api_url, headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+    for attempt in range(max_retries):
+        try:
+            print(f"开始请求 {attempt}")
+            response = requests.post(api_url, headers=headers, json=data)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"请求失败 (尝试 {attempt + 1}/{max_retries}): {e}")
+            if attempt + 1 == max_retries:  # 如果是最后一次尝试，返回错误信息
+                return {"error": str(e)}
+            time.sleep(2 ** attempt)  # 指数退避，增加重试间隔
+
 
 def sanitize_input(text):
     """Remove or replace problematic characters from the input."""
@@ -72,9 +83,15 @@ def process_row(row, auth_token):
     content_description = row['内容描述']
     description = row['description']
 
+    # 判断 cloud_platform 是否等于指定的平台，不区分大小写
+    if cloud_platform.lower() != specify_cloud_platform.lower():
+        print(f"跳过处理: {name}，因为云平台不匹配")
+        return name, "云平台不匹配"
+    
     prompt = sanitize_input(
         f"云服务检测项内容为：{cloud_platform} {name} {rules} {description},"
-        f"最匹配哪个CIS Controls v8的requirements."
+        #f"最匹配哪个CIS aws benchmark v3的recomandation."
+        f"最匹配哪个CIS aws benchmark v3的recomandation. 要求recomandation所属的云产品必须是{scan_type}，没有匹配的返回'无对应云服务产品'."
     )
 
     #print(f"Sending prompt: {prompt}")  # Log the prompt   
