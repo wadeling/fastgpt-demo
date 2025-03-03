ee930216f4f09978643cb2cc70415a31242905f9
wadeling <wadeling@gmail.com>
2025-03-03 17:20:25 +0800

    add azure cis benchmark v3

--- compliance_analyze.py
+++ compliance_analyze.py
@@ -5,7 +5,7 @@ import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # specify cloud platform
-specify_cloud_platform = 'aws'
+specify_cloud_platform = 'azure'
 
 
 # framework
@@ -18,7 +18,7 @@ api_url = 'https://cloud.fastgpt.cn/api/v1/chat/completions'
 auth_token_file = f'{framework}_app_key'
 
 # 定义CSV文件路径
-test_csv_file_path = 'plugin_aws_failed.csv'
+test_csv_file_path = 'plugin_azure_failed.csv'
 out_csv_file_path = f'plugin_with_{framework}_{specify_cloud_platform}_deepseek_reasoner.csv'
 
 concurrent_num = 20
@@ -35,7 +35,7 @@ def read_token_file(file_path):
     except IOError:
         return f"读取文件时发生错误: {file_path}"
 
-def send_chat_request(api_url, auth_token, chat_id, user_message, stream=False, detail=False):
+def send_chat_request(name,api_url, auth_token, chat_id, user_message, stream=False, detail=False):
     """发送聊天请求到指定的API接口。"""
     headers = {
         'Authorization': f'Bearer {auth_token}',
@@ -56,12 +56,12 @@ def send_chat_request(api_url, auth_token, chat_id, user_message, stream=False, 
 
     for attempt in range(max_retries):
         try:
-            print(f"开始请求 {attempt}")
+            print(f"开始请求 {attempt} {name}")
             response = requests.post(api_url, headers=headers, json=data)
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"请求失败 (尝试 {attempt + 1}/{max_retries}): {e}")
+            print(f"请求失败 {name} (尝试 {attempt + 1}/{max_retries}): {e}")
             if attempt + 1 == max_retries:  # 如果是最后一次尝试，返回错误信息
                 return {"error": str(e)}
             time.sleep(2 ** attempt)  # 指数退避，增加重试间隔
@@ -73,7 +73,6 @@ def sanitize_input(text):
 
 def process_row(row, auth_token):
     """处理每一行数据并返回处理结果。"""
-    print(f"process row:{row}")
 
     name = row['name']
     scan_item = row['扫描项']
@@ -83,6 +82,8 @@ def process_row(row, auth_token):
     content_description = row['内容描述']
     description = row['description']
 
+    print(f"process row name:{name}")
+
     # 判断 cloud_platform 是否等于指定的平台，不区分大小写
     if cloud_platform.lower() != specify_cloud_platform.lower():
         print(f"跳过处理: {name}，因为云平台不匹配")
@@ -91,12 +92,12 @@ def process_row(row, auth_token):
     prompt = sanitize_input(
         f"云服务检测项内容为：{cloud_platform} {name} {rules} {description},"
         #f"最匹配哪个CIS aws benchmark v3的recomandation."
-        f"最匹配哪个CIS aws benchmark v3的recomandation. 要求recomandation所属的云产品必须是{scan_type}，没有匹配的返回'无对应云服务产品'."
+        f"最匹配哪个CIS microsoft azure benchmark v3的recomandation. 要求recomandation所属的云产品必须是{scan_type}，没有匹配的返回'无对应云服务产品'."
     )
 
     #print(f"Sending prompt: {prompt}")  # Log the prompt   
     chat_id = str(int(time.time()))
-    result = send_chat_request(api_url, auth_token, chat_id, prompt)
+    result = send_chat_request(name,api_url, auth_token, chat_id, prompt)
 
     if "error" in result:
         return name, "请求失败"
